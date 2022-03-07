849e39bb5ad8e00904fcb16df76071844468c6e0
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-07 15:10:45 -0600

    updating location and choose-deck styling

--- game.js
+++ game.js
@@ -5,7 +5,29 @@ console.log('linked to play');
 
 // Functions
 
+// Direction for changing p1 and p2 images (function changeP1Image, changeP2Image) taken from https://stackoverflow.com/questions/27722485/change-a-image-when-list-box-selected-item-is-changed
+
+function changeP1Image() {
+    let duckList = document.getElementById('Player-1-choice');
+    let idx = duckList.selectedIndex;
+    let duckImageName = duckList[idx].value;
+    console.log(duckImageName);
+    let playerPic = document.getElementById("Player-1-pic");
+    playerPic.setAttribute('src', `assets/player-pictures/player-1/${duckImageName}.png`)
+
+}
+
+function changeP2Image() {
+    let duckList = document.getElementById('Player-2-choice');
+    let idx = duckList.selectedIndex;
+    let duckImageName = duckList[idx].value;
+    console.log(duckImageName);
+    let playerPic = document.getElementById("Player-2-pic");
+    playerPic.setAttribute('src', `assets/player-pictures/player-2/${duckImageName}.png`)
+}
 // Event Listeners
+document.getElementById('Player-1-choice').addEventListener('change', changeP1Image);
+document.getElementById('Player-2-choice').addEventListener('change', changeP2Image);
 
 // JS for play.thml
 

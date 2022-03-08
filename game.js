9ade5d7cc596bec9e9b11d14a33ea371222a57d8
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-08 11:09:42 -0600

    continued work on player profiles in play.html w/ updated profile pics

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ let player1Duck = localStorage.getItem('player1Duck');
 let player2Duck = localStorage.getItem('player2Duck');
 console.log(player1Duck);
 console.log(player2Duck);
+let player1Pic;
+let player2Pic;
 // pass data for profile image
 
 // Class Objects
@@ -16,6 +18,10 @@ if(player1Duck !== null && player2Duck !== null) {
 // Functions
 function gameInit() {
 // Set player profile pictures:
+player1Pic = document.getElementById("Player-1-pic-play");
+player1Pic.setAttribute('src', `assets/player-pictures/player-1/${player1Duck}.png`)
+player2Pic = document.getElementById("Player-2-pic-play");
+player2Pic.setAttribute('src', `assets/player-pictures/player-2/${player2Duck}.png`)
 // let playerPic = document.getElementById("Player-2-pic");
 // playerPic.setAttribute('src', `assets/player-pictures/player-2/${duckImageName}.png`)
 // Set score to 0

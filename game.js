def7273282119a9e4c7dbf37d210c885c18bc740
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-08 13:23:43 -0600

    redefined player1 and 2 variables as objects for more complex variable storage

--- game.js
+++ game.js
@@ -1,18 +1,29 @@
 // Global Variables
-const player1 = 'player1' // change to object?
-const player2 = 'player2' // change to object?
 let gameRound;
 let currentPlayer;
-let player1Score;
-let player2Score;
+// let player1Score;
+// let player2Score;
 let gameStatus = false;
-let player1Pic;
-let player2Pic;
 
 // pass data for deck select and profile image
-let player1Duck = localStorage.getItem('player1Duck');
-let player2Duck = localStorage.getItem('player2Duck');
+// let player1Duck = localStorage.getItem('player1Duck');
+// let player2Duck = localStorage.getItem('player2Duck');
 
+
+// Player Objects
+const player1 = {
+    name: 'Player 1',
+    score: 0,
+    // pass data for deck select and profile image
+    duck: localStorage.getItem('player1Duck')
+}
+
+const player2 = {
+    name: 'Player 2',
+    score: 0,
+    // pass data for deck select and profile image
+    duck: localStorage.getItem('player2Duck')
+}
 
 // Class Objects
 
@@ -21,12 +32,12 @@ gameInit();
 
 // Functions
 function gameInit() {
-    if(player1Duck !== null && player2Duck !== null) {
+    if(player1.duck !== null && player2.duck !== null) {
     gameStatus = true;
 
     // Set player Scores to 0;
-    player1Score = 0;
-    player2Score = 0;
+    // player1Score = 0;
+    // player2Score = 0;
 
     // Set Rounds to 1
     gameRound = 1;
@@ -48,43 +59,43 @@ function coinToss() {
     } else {
         currentPlayer = player2;
     }
-    console.log(`${currentPlayer} goes first`);
+    console.log(`${currentPlayer.name}: ${currentPlayer.duck} go first`);
 }
 
     function buildP1Deck() {
         // Set player profile pictures:
-        if(player1Duck === 'Random') {
+        if(player1.duck === 'Random') {
             let randomSelect = Math.floor(Math.random()*3);
             if(randomSelect === 0) {
-                player1Duck = 'GreyDucks';
+                player1.duck = 'GreyDucks';
             } else if (randomSelect === 1) {
-                player1Duck = 'Yellers';
+                player1.duck = 'Yellers';
             } else if (randomSelect === 2) {
-                player1Duck = 'Mallards';
+                player1.duck = 'Mallards';
             }
         }
-        player1Pic = document.getElementById("Player-1-pic-play");
-        player1Pic.setAttribute('src', `assets/player-pictures/player-1/${player1Duck}.png`)
-        console.log(player1Duck);
+        player1.picture = document.getElementById("Player-1-pic-play");
+        player1.picture.setAttribute('src', `assets/player-pictures/player-1/${player1.duck}.png`)
+        console.log(player1.duck);
         // Randomly deal cards to each player
     };
 
     function buildP2Deck() {
          // Set player profile pictures:
-         if(player2Duck === 'Random') {
+         if(player2.duck === 'Random') {
             let randomSelect = Math.floor(Math.random()*3);
             if(randomSelect === 0) {
-                player2Duck = 'GreyDucks';
+                player2.duck = 'GreyDucks';
             } else if (randomSelect === 1) {
-                player2Duck = 'Yellers';
+                player2.duck = 'Yellers';
             } else if (randomSelect === 2) {
-                player2Duck = 'Mallards';
+                player2.duck = 'Mallards';
             }
         }
-        player2Pic = document.getElementById("Player-2-pic-play");
-        player2Pic.setAttribute('src', `assets/player-pictures/player-2/${player2Duck}.png`)
+        player2.picture = document.getElementById("Player-2-pic-play");
+        player2.picture.setAttribute('src', `assets/player-pictures/player-2/${player2.duck}.png`)
        // Randomly deal cards to each player
-       console.log(player2Duck);
+       console.log(player2.duck);
     };
 
 // Event Handlers
